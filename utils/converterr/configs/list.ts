f4c029ad1dd2e5ed6c5c9cce5ccc8514a7a9b5cc
hainv-tda <nvhai0111.work@gmail.com>
2025-05-14 09:49:44 +0700

    update

--- utils/converterr/configs/list.ts
+++ utils/converterr/configs/list.ts
@@ -4,7 +4,7 @@ const ruleConfigs: IRuleConfig[] = [
   {
     type: ERuleConfigType.WRAP,
     detected:
-      "(?<=<c:import[^>]*section_title[^>]*>%any%*?</c:import>)(%any%+?)(?=(<!-- footer start -->)?%space%*<c:import[^>]*footer[^>]*>)",
+      "(?<=<c:import[^>]*section_title[^>]*>%any%*?</c:import>)(%any%+?)(?=(?:(<!-- footer start -->)?%space%*<c:import[^>]*footer[^>]*>|<!-- content_main end -->))",
     dataReplaced: ` 
     <main class="asis-main">
         <div class="asis-content asis-content--list">
